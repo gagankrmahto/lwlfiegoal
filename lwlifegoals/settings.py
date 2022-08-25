314bbc765597b94c515f2c6f1367c630f196816b
Gagan Kumar Mahto <gagan0966@gmail.com>
2022-08-25 12:19:35 +0530

    fixed db issue

--- lwlifegoals/settings.py
+++ lwlifegoals/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'lwlifegoals.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db/db.sqlite3',
+        'NAME': BASE_DIR / 'db'/'db.sqlite3',
     }
 }
 
